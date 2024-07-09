498ca942510c0afc5329caf5663e7c4907e8bf03
matheus kamers <matheus.kamers@checkstore.com.br>
2024-07-09 19:21:33 -0300

    feature: add get a list of products route

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -6,6 +6,7 @@ const productRoutes = Router();
 const productController = new ProductController();
 
 productRoutes.post('/product', productController.create);
+productRoutes.get('/product', productController.getProducts);
 
 
 export default productRoutes;
